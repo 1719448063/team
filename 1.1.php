4a94cc55403dea3358a09e3072cdb5345c8cd15f
root <1719448063@qq.com>
2018-11-29 20:35:42 +0800

    1.1.php

---
+++ 1.1.php
@@ -0,0 +1,39 @@
+<table border="1" align="left">
+	<tr>
+		<td>员工姓名</td>
+		<td><input type="text"></td>
+	</tr>
+	<tr>
+		<td>密码</td>
+		<td><input type="password"></td>
+	</tr>
+	<tr>
+		<td>确认密码：</td>
+		<td><input type="password"></td>
+	</tr>
+	<tr>
+		<td>年龄：</td>
+		<td>
+			<select>
+				<option>
+					18
+				</option>
+				<option>
+					19
+				</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>性别：</td>
+		<td>
+			<input type="radio">男
+			<input type="radio">女
+			</td>
+	</tr>
+	<tr>
+		
+		<td><input type="button" value="添加"></td>
+		<td></td>
+	</tr>
+</table>
